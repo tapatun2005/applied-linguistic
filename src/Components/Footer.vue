32d5c27ce02a988348a5b0607ae5fb75339adc98
Vlad Kuzmin <vlad.kuzmin@epiphany.co.uk>
2021-03-07 18:40:00 +0000

    [LINK] buy link

--- src/Components/Footer.vue
+++ src/Components/Footer.vue
@@ -16,7 +16,7 @@
                             </h2>
                             <p>This book explores the mechanics of storytelling within a study aimed at focusing on a ‘hidden’ population of migrants in the city of York, UK. Taking applied linguistics to mean the consideration of real-world ‘problems’ as identified by a ‘client’, in which the use of (and beliefs about) language is a significant component, the authors describe the benefits and challenges of working in a partnership with a community organisation. With project participants from Africa, Europe, Asia and South and Central America who had lived in York between two and fifty years, the study considers the co-construction of meaning in interviews from a range of practical and theoretical perspectives. The book will be of interest to students, academic researchers and community project leaders who are interested in migration stories and interviews as a method of data collection.</p>
 
-                            <a href="https://www.amazon.co.uk/Yorks-Hidden-Stories-Interviews-Linguistics-ebook/dp/B08M5YJV43/ref=sr_1_1?dchild=1&keywords=zhurauskaya&qid=1613596545&sr=8-1" class="btn -green -sm" target="_blank">Buy</a>
+                            <a href="https://www.palgrave.com/gp/book/9781137558381" class="btn -green -sm" target="_blank">Buy</a>
                         </article>
                 </div>
             </div>
